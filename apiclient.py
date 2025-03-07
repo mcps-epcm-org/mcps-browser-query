555efddc76ea3e7ec1ad7f09e43167ffd2a1af07
Fernando Rivas <fenandos@hotmail.com>
2025-03-07 16:24:20 -0600

    Add headers..

--- apiclient.py
+++ apiclient.py
@@ -4,19 +4,27 @@ apiclient.py
 API 
 
 """
+
 import os
 from typing import Any, Dict
 import requests
 from models import ResponseModel
 
+
 class APIClient:
     """
     Cliente para realizar consultas a la API.
     """
+
     def __init__(self, base_url: str):
         self.base_url = base_url
         self.api_key = os.getenv("MCPS_BROWSER_API_KEY")
-    
+        self.headers = {
+            "Accept": "application/json, text/plain, /",
+            "Referer": "https://rgc-mcps.regeneron.com/",
+            "x-api-key": "pHBUpmf3hE2d9LKHQvgLY2LKMmW1Cns15E8JDsNn",
+        }
+
     def fetch_data(self, endpoint: str) -> ResponseModel:
         url = f"{self.base_url}/{endpoint}"
         response = requests.get(url)
